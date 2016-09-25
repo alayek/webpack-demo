2a28d1790b866e8ac45758d2701c7599a1e45607
alayek <alayek@engineer.com>
2016-09-25 23:51:10 +0530

    Add stats support

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,7 @@ const common = {
 let config;
 
 switch(process.env.npm_lifecycle_event) {
+	case 'stats':
 	case 'build':
 		config = merge(
 			common, 
@@ -78,7 +79,9 @@ switch(process.env.npm_lifecycle_event) {
 		);
 }
 
-module.exports = validate(config);
+module.exports = validate(config, {
+	quiet: true
+});
 
 
 
